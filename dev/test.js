c435e8a72fad35b93f8d7d876338e30e09039d3e
Raymond Ly <workrayz@gmail.com>
2019-03-04 18:42:30 +0000

    create blockchain

---
+++ dev/test.js
@@ -0,0 +1,14 @@
+const Blockchain = require("./blockchain");
+
+const bitcoin = new Blockchain();
+
+// const previousBlockHash = "87765DA6CCF0668238C1D27C35692E11";
+// const currentBlockData = [
+//   { amount: 101, sender: "B4CEE9C0E5CD571", recipient: "3A3F6E462D48E9" },
+//   { amount: 30, sender: "3A3F6E462D48E9", recipient: "FD98FD78A9SD8F" },
+//   { amount: 200, sender: "09AD8F9809AS980", recipient: "FD980A8098WQ8R" }
+// ];
+// const nonce = 111767;
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+
+console.log(bitcoin);

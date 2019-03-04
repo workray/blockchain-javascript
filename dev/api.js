8f558a7ac4759ec7e4079e7f2c7ad4a7a324ab05
Raymond Ly <workrayz@gmail.com>
2019-03-04 19:12:02 +0000

    Building the /transaction endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,31 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
+const bitcoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+app.get("/blockchain", function(req, res) {
+  res.send(bitcoin);
+});
+
+app.post("/transaction", function(req, res) {
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ note: `Transaction will be added in block ${blockIndex}` });
+});
+
+app.get("/mine", function(req, res) {});
+
+app.listen(3000, function() {
+  console.log("listening on port 3000...");
+});
